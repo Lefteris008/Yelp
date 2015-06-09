1c9045983fa18e7a5a133ac3b9cfedc09f28d4f0
Alex <Alex@ATG.lan>
2015-06-10 00:59:22 +0300

    Merge origin/master
    Conflicts:
    	src/yelp/GetData.java

---
+++ src/yelp/FilePaths.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package yelp;
+
+/**
+ *
+ * @author Alex
+ */
+public class  FilePaths {
+    
+    public static String businessFilePath = "C:\\Users\\lefte_000\\Downloads\\yelp\\yelp_academic_dataset_business.json";
+    public static String checkinFilePath = "C:\\Users\\lefte_000\\Downloads\\yelp\\yelp_academic_dataset_checkin.json";
+    
+}
