ec02cf50a0d742e32056d2f6965e985e4d6221c3
Alex <Alex@ATG>
2015-06-22 22:02:18 +0300

    Merge origin/master
    Conflicts:
    	src/yelp/GetData.java

--- src/yelp/Query.java
+++ src/yelp/Query.java
@@ -91,7 +91,7 @@ public class Query {
 
     private static String stringQuery(ArrayList list) {
         String sqlStmt;
-        sqlStmt = sqlStmt = "SELECT business_id, checkin_day, checkin_time, checkin_count, business_location.business_name, business_location.latitude, business_location.longitude, business_location.stars, difference\n"
+        sqlStmt =  "SELECT business_id, checkin_day, checkin_time, checkin_count, business_location.business_name, business_location.latitude, business_location.longitude, business_location.stars, difference\n"
                 + "FROM business_location\n"
                 + "INNER JOIN\n"
                 + "	(SELECT b.business_id, b.checkin_day, b.checkin_time, b.checkin_count, b.checkin_count - t.checkin_count AS difference\n"
